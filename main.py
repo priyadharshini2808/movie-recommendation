b2bafbf8dd5a52a14928411f7a55570f22714e78
Priyadharshini K <61087664+priyadharshini2808@users.noreply.github.com>
2020-05-16 12:51:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+data=pd.read_csv("C:/Users/elcot/Downloads/movie_metadata.csv")
+data.columns
+data = data.loc[:,['actor_1_name','actor_2_name','actor_3_name','director_name','genres','movie_title']]
+data.head()
+data.isnull().sum(axis=0)
+data['actor_1_name'] = data['actor_1_name'].replace(np.nan, 'unknown')
+data['actor_2_name'] = data['actor_2_name'].replace(np.nan, 'unknown')
+data['actor_3_name'] = data['actor_3_name'].replace(np.nan, 'unknown')
+data['director_name'] = data['director_name'].replace(np.nan, 'unknown')
+data['genres'] = data['genres'].replace('|', ' ')
+data['movie_title'] = data['movie_title'].str.lower()
+data['movie_title'][0]
+data['movie_title'] = data['movie_title'].str[:-1]
+data.to_csv('data.csv',index=False)
+from flask import Flask, render_template, request
+# libraries for making count matrix and similarity matrix
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# define a function that creates similarity matrix
+# if it doesn't exist
+def create_sim():
+    data = pd.read_csv('data.csv')
+    # creating a count matrix
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    # creating a similarity score matrix
+    sim = cosine_similarity(count_matrix)
+    return data,sim
+
+
+# defining a function that recommends 10 most similar movies
+def rcmd(m):
+    m = m.lower()
+    # check if data and sim are already assigned
+    try:
+        data.head()
+        sim.shape
+    except:
+        data, sim = create_sim()
+    # check if the movie is in our database or not
+    if m not in data['movie_title'].unique():
+        return('This movie is not in our database.\nPlease check if you spelled it correct.')
+    else:
+        # getting the index of the movie in the dataframe
+        i = data.loc[data['movie_title']==m].index[0]
+
+        # fetching the row containing similarity scores of the movie
+        # from similarity matrix and enumerate it
+        lst = list(enumerate(sim[i]))
+
+        # sorting this list in decreasing order based on the similarity score
+        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+
+        # taking top 1- movie scores
+        # not taking the first index since it is the same movie
+        lst = lst[1:11]
+
+        # making an empty list that will containg all 10 movie recommendations
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/recommend")
+def recommend():
+    movie = request.args.get('movie')
+    r = rcmd(movie)
+    movie = movie.upper()
+    if type(r)==type('string'):
+        return render_template('recommend.html',movie=movie,r=r,t='s')
+    else:
+        return render_template('recommend.html',movie=movie,r=r,t='l')
+
+
+
+if __name__ == '__main__':
+    app.run()
